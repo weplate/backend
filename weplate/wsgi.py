0356c37dc7453c626edeec746c4fe23e2b8f42cc
Moses Xu <moses@mosesxu.net>
2022-01-26 00:33:15 -0500

    fix backend (manage.py was not working)

--- weplate/wsgi.py
+++ weplate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for weplated project.
+WSGI config for weplate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weplated.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weplate.settings')
 
 application = get_wsgi_application()
