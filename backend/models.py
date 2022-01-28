3febb1f038f6246a3b515c984b35cdf7474f3649
Moses Xu <moses@mosesxu.net>
2022-01-27 21:28:24 -0500

    I know this is bad practice but I want to work on this more on my pc so I'm just gonna commit some code and pull it

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from collections import namedtuple
+
 
 # Create your models here.
+class NutritionalInfo:
+    PropertyInfo = namedtuple('PropertyInfo', 'displayname unit default')
+
+    units = {
+        'ug': 10**-6,
+        'mg': 10**-3,
+        'g': 1.,
+        'kg': 1000.,
+    }
+
+    @classmethod
+    def convert_unit(cls, value, f, t):
+        if f not in cls.units: raise ValueError(f'Invalid Unit {f}')
+        if t not in cls.units: raise ValueError(f'Invalid Unit {t}')
+
+        return value * (cls.units[f] / cls.units[t])
+
+    nutrients = {
+        'calories': PropertyInfo('Calories (cal)', 'g', 0)  # I know calories is not in grams, but no conversions need to be done anyway, so this should be ok
+    }
+
+    def __init__(self, obj={}):
+        """
+        :param obj: Construct this from JSON
+        """
+        pass
+
+    def json(self):
+        pass
+
+class MealOption(models.Model):
+    pass
+
+class MealSelection(models.Model):
+    pass
+
+class School(models.Model):
+    pass
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
