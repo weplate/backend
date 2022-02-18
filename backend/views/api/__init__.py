b0424f96f21af322470fa7d86a8f703196b2dd38
Moses Xu <moses@mosesxu.net>
2022-02-17 21:32:54 -0500

    working on meal planning things

--- backend/views/api/__init__.py
+++ backend/views/api/__init__.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
 from .info import SchoolViewSet, IngredientViewSet, MealSelectionViewSet, SchoolMealItemsViewSet
-from .meal_planning import NutritionalRequirementsViewSet
+from .meal_planning import NutritionalRequirementsViewSet, SuggestViewSet
 from .register import register_student_view
 from .settings import SettingsViewSet
 
@@ -12,8 +12,10 @@ router.register(r'schools', SchoolViewSet)
 router.register(r'ingredients', IngredientViewSet, basename='Ingredient')
 router.register(r'school_items', SchoolMealItemsViewSet, basename='SchoolMealItems')
 router.register(r'meals', MealSelectionViewSet, basename='MealSelection')
-router.register(r'nutritional_requirements', NutritionalRequirementsViewSet, basename='NutritionalRequirements'),
 router.register(r'settings', SettingsViewSet, basename='Settings')
+
+router.register(r'nutritional_requirements', NutritionalRequirementsViewSet, basename='NutritionalRequirements')
+router.register(r'suggest', SuggestViewSet, basename='SuggestViewSet')
 
 urlpatterns = [
     path('token_auth/', obtain_auth_token),
--- backend/views/api/meal_planning.py
+++ backend/views/api/meal_planning.py
@@ -1,10 +1,14 @@
-from rest_framework import views, viewsets
+from rest_framework import viewsets, serializers
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.exceptions import APIException
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 from rest_framework.response import Response
 
-from backend.algorithm import nutritional_info_for
-from backend.models import StudentProfile
+from backend.algorithm import nutritional_info_for, LARGE_PORTION_MAX, MIN_FILL, SMALL_PORTION_MAX
+from backend.models import StudentProfile, MealItem, MealSelection
 from backend.views.api.info import ReadNutritionalInfoSerializer
 from backend.views.common import IsStudent
 
@@ -16,3 +20,44 @@ class NutritionalRequirementsViewSet(viewsets.ViewSet):
     def list(self, request):
         profile = StudentProfile.objects.get(user=request.user)
         return Response(ReadNutritionalInfoSerializer(nutritional_info_for(profile)).data)
+
+
+class PortionRequestSerializer(serializers.Serializer):
+    small1 = serializers.PrimaryKeyRelatedField(queryset=MealItem.objects.all())
+    small2 = serializers.PrimaryKeyRelatedField(queryset=MealItem.objects.all())
+    large = serializers.PrimaryKeyRelatedField(queryset=MealItem.objects.all())
+
+
+class SuggestViewSet(viewsets.ViewSet):
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsStudent]
+
+    @action(methods=['GET'], detail=True)
+    def items(self, request: Request, pk: int):
+        meal = get_object_or_404(MealSelection, pk=pk)
+
+    @action(methods=['GET'], detail=False)
+    def portions(self, request: Request):
+        req_ser = PortionRequestSerializer(data=request.query_params)
+        small1 = req_ser.validated_data['small1']
+        small2 = req_ser.validated_data['small2']
+        large = req_ser.validated_data['large']
+
+        tmp_sh = SMALL_PORTION_MAX * MIN_FILL
+        tmp_lh = LARGE_PORTION_MAX * MIN_FILL
+
+        return Response({
+            'small1': {
+                'volume': tmp_sh,
+                'weight': tmp_sh * small1.density()
+            },
+            'small2': {
+                'volume': tmp_sh,
+                'weight': tmp_sh * small2.density()
+            },
+            'large': {
+                'volume': tmp_lh,
+                'weight': tmp_lh * large.density()
+            },
+            'quality': 69
+        })
