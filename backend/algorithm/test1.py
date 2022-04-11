3974aeae51a2b9897be01594c424cec59040325c
pen1064 <penelope.kwong@mail.utoronto.ca>
2022-04-11 13:24:07 -0400

    MODIFY

--- backend/algorithm/test1.py
+++ backend/algorithm/test1.py
@@ -38,53 +38,6 @@ def item_choice_example():
     algo.result_obj()  # Check this for result
 
 
-def item_portion_example(height):#, weight, birthdate, meals, meal_lenth, sex, health_goal, activity_level):
-    profile = StudentProfileSpec(
-        height=height,
-        weight=80,
-        birthdate=datetime.date(year=2003, month=11, day=30),
-        meals=['breakfast', 'lunch'],
-        meal_length=50,
-        sex=MALE,
-        health_goal=ATHLETIC_PERFORMANCE,
-        activity_level=MODERATE
-    )
-    lo_req, hi_req = nutritional_info_for(profile)
-    algo = SimulatedAnnealing(
-        lo_req=lo_req,
-        hi_req=hi_req,
-        state=[
-            PlateSectionState(
-                nutrition=Nutrition(
-                    # nutrition facts
-                ),
-                portion_volume=100,
-                discrete=False,
-                max_volume=610,
-                id='your mom'
-            ),
-            PlateSectionState(
-                nutrition=Nutrition(
-                    # nutrition facts
-                ),
-                portion_volume=5,  # Number of pieces in a portion
-                discrete=True,
-                max_volume=10,  # Max num. of pieces
-                id='penelopeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-            )  # more obviously...
-        ],
-        alpha=0.999,
-        smallest_temp=0.0005,
-        seed=20210101
-    )
-    algo.run_algorithm()
-
-    # Final results are in algo.state
-    for state in algo.state:
-        pass  # final volume is in state.volume, item id is in state.id.  Use state.discrete to check if the item is discrete or not
-    return algo.state
-
-
 def item_portion_example1(height):#, weight, birthdate, meals, meal_lenth, sex, health_goal, activity_level):
     profile = StudentProfileSpec(
         height=height,
@@ -130,6 +83,7 @@ def item_portion_example1(height):#, weight, birthdate, meals, meal_lenth, sex, 
     for state in algo.state:
         pass  # final volume is in state.volume, item id is in state.id.  Use state.discrete to check if the item is discrete or not
     return algo.state
+
 
 if __name__ == '__main__':
     height = 1.78
