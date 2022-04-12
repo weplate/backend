6dac64d3e69afa8722be63f46e7e93fc6c5254c4
pen1064 <penelope.kwong@mail.utoronto.ca>
2022-04-12 17:28:12 -0400

    test_1

--- backend/algorithm/test1.py
+++ backend/algorithm/test1.py
@@ -4,15 +4,21 @@ sys.path[0] = str(Path(sys.path[0]).parent.parent)
 print(sys.path[0])
 import datetime
 import pandas as pd
+import numpy as np
 import os
-from backend.algorithm.common import ATHLETIC_PERFORMANCE, MALE, MODERATE, Nutrition, VEGETABLE
+from dataclasses import fields
+from datetime import date
+
+from backend.algorithm.common import * 
 from backend.algorithm.item_choice import MealItemSelector
 from backend.algorithm.portion import SimulatedAnnealing, PlateSectionState, MealItemSpec
 from backend.algorithm.requirements import StudentProfileSpec, nutritional_info_for
 
-
+NUTRIENTS = ['calories', 'carbohydrate', 'protein','total_fat','saturated_fat', 'trans_fat',
+'sugar', 'cholesterol', 'fiber','sodium', 'potassium', 'calcium','iron', 'vitamin_a', 'vitamin_c', 'vitamin_d']
 NUTRITION_TABLE_LATEST = os.path.join(sys.path[0], 'backend_data_parsing/babson/master_nutrition/nutrition_table.csv')
-MENU_LATEST= os.path.join(sys.path[0], 'backend_data_parsing/babson/master_menu/menu.csv')
+MENU_LATEST= os.path.join(sys.path[0], 'backend_data_parsing/babson/master_menu/old_file/menu.csv')
+STUDENT = r'C:\Users\Penelope\Desktop\ML\Weplate\synthetic_data\fake_data_shorter.csv'
 
 def item_choice_example(height, weight, birthdate, meals, meal_length, sex, health_goal, activity_level, meal_items):
     profile = StudentProfileSpec(height=height,weight=weight,birthdate=birthdate,
@@ -29,9 +35,10 @@ def item_choice_example(height, weight, birthdate, meals, meal_length, sex, heal
     )
     algo.run_algorithm()
     algo.result_obj()  # Check this for result
+    return algo.result_obj()
 
 
-def item_portion_example(height):#, weight, birthdate, meals, meal_lenth, sex, health_goal, activity_level):
+def item_portion_example_old(height):#, weight, birthdate, meals, meal_lenth, sex, health_goal, activity_level):
     profile = StudentProfileSpec(
         height=height,
         weight=80,
@@ -120,41 +127,14 @@ def item_portion_example(height):#, weight, birthdate, meals, meal_lenth, sex, h
     return algo.state
 
 
-def item_portion_example(height):#, weight, birthdate, meals, meal_lenth, sex, health_goal, activity_level):
-    profile = StudentProfileSpec(
-        height=height,
-        weight=80,
-        birthdate=datetime.date(year=2003, month=11, day=30),
-        meals=['breakfast', 'lunch'],
-        meal_length=50,
-        sex=MALE,
-        health_goal=ATHLETIC_PERFORMANCE,
-        activity_level=MODERATE
-    )
+def item_portion_example(height, weight, birthdate, meals, meal_length, sex, health_goal, activity_level, state):
+    profile = StudentProfileSpec(height=height,weight=weight,birthdate=birthdate,
+    meals=meals,meal_length=meal_length,sex=sex,health_goal=health_goal,activity_level=activity_level)
     lo_req, hi_req = nutritional_info_for(profile)
     algo = SimulatedAnnealing(
         lo_req=lo_req,
         hi_req=hi_req,
-        state=[
-            PlateSectionState(
-                nutrition=Nutrition(
-                    # nutrition facts
-                ),
-                portion_volume=100,
-                discrete=False,
-                max_volume=610,
-                id='your mom'
-            ),
-            PlateSectionState(
-                nutrition=Nutrition(
-                    # nutrition facts
-                ),
-                portion_volume=5,  # Number of pieces in a portion
-                discrete=True,
-                max_volume=10,  # Max num. of pieces
-                id='penelopeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
-            )  # more obviously...
-        ],
+        state=state,
         alpha=0.999,
         smallest_temp=0.0005,
         seed=20210101
@@ -166,57 +146,159 @@ def item_portion_example(height):#, weight, birthdate, meals, meal_lenth, sex, h
         pass  # final volume is in state.volume, item id is in state.id.  Use state.discrete to check if the item is discrete or not
     return algo.state
 
+def retrieve_result(algo_portion_state):
+        data_combination = []
+        portion_volume = []
+        for s in algo_portion_state:
+            dish_nutrient = []
+            for nutrient in NUTRIENTS:
+                dish_nutrient.append(getattr(s.nutrition, nutrient))
+            data_combination.append(dish_nutrient)
+            portion_volume.append(s.volume)
+        data_combination = np.array(data_combination)
+        
+        return portion_volume + list(sum(data_combination)) 
+
 if __name__ == '__main__':
     master_nutrition_df = pd.read_csv(NUTRITION_TABLE_LATEST, 
     converters={'cafeteria_id': lambda x: str(x), 'category': lambda x: str(x)})
     master_menu_df = pd.read_csv(MENU_LATEST, 
     converters={'cafeteria_id': lambda x: str(x), 'category': lambda x: str(x)})
-    height = 1.78
-    weight = 80
-    birthdate = datetime.date(year=2003, month=11, day=30)
-    meals = ['breakfast', 'lunch']
-    meal_length = 50
-    sex = MALE
-    health_goal = ATHLETIC_PERFORMANCE
-    activity_level = MODERATE
+    student_df = pd.read_csv(STUDENT)
+    student_df['Birthday'] = pd.to_datetime(student_df['Birthday'])
 
-    #for ind, menu_row in master_menu_df.iterrows():
-        #menu = eval(menu_row['items']) #pk
-    menu = [1047, 1021, 1014, 1036, 1037, 1056, 1040]
-    if len(menu) > 0:
-        meal_items = []
-        short_df = pd.DataFrame()
-        short_list_df = master_nutrition_df[master_nutrition_df['pk'].isin(menu)]
-        for ind, row in short_list_df.iterrows():
-            id = row['pk']
-            cafeteria_id= row['cafeteria_id']
-            category = row['category']
-            portion_volume = float(row['portion_volume'])
-            max_pieces = row['max_pieces']
-            carbohydrate = float(row['carbohydrate'])
-            calories = float(row['calories'])
-            protein = float(row['protein'])
-            total_fat = float(row['total_fat'])
-            saturated_fat = float(row['saturated_fat'])
-            sugar= float(row['sugar'])
-            cholesterol=float(row['cholesterol'])
-            fiber= float(row['fiber'])
-            sodium=float(row['sodium'])
-            potassium=float(row['potassium'])
-            calcium=float(row['calcium'])
-            iron=float(row['iron'])
-            vitamin_a=float(row['vitamin_a'])
-            vitamin_c=float(row['vitamin_c'])
-            vitamin_d=float(row['vitamin_d'])
+    for ind_student, student_row in student_df.iterrows():
+        print('student-index:', ind_student)
+        height = student_row['Height']
+        weight = student_row['Weight']
+        sex = eval(student_row['Sex'])
+        meal_length = 50
+        health_goal = eval(student_row['Health_Goal'].upper())
+        activity_level = eval(student_row['Activity'].upper())
+        yr = student_row['Birthday'].year
+        mon = student_row['Birthday'].month
+        dat = student_row['Birthday'].day
+        birthdate = datetime.date(year=yr, month=mon, day=dat)
+        meals = ['breakfast', 'lunch']
+        age = 2022 - yr
+        DATA = []
+        for ind, menu_row in master_menu_df.iterrows():
+            day = menu_row['timestamp']
+            meal_name = menu_row['name']
+            menu = eval(menu_row['items']) #pk
+        #menu = [1047, 1021, 1014, 1036, 1037, 1056, 1040]
+            if len(menu) > 0:
+                meal_items = []
+                short_df = pd.DataFrame()
+                short_list_df = master_nutrition_df[master_nutrition_df['pk'].isin(menu)]
+                for ind, row in short_list_df.iterrows():
+                    id = row['pk']
+                    cafeteria_id= row['cafeteria_id']
+                    category = row['category']
+                    portion_volume = float(row['portion_volume'])
+                    max_pieces = row['max_pieces']
+                    carbohydrate = float(row['carbohydrate'])
+                    calories = float(row['calories'])
+                    protein = float(row['protein'])
+                    total_fat = float(row['total_fat'])
+                    saturated_fat = float(row['saturated_fat'])
+                    sugar= float(row['sugar'])
+                    cholesterol=float(row['cholesterol'])
+                    fiber= float(row['fiber'])
+                    sodium=float(row['sodium'])
+                    potassium=float(row['potassium'])
+                    calcium=float(row['calcium'])
+                    iron=float(row['iron'])
+                    vitamin_a=float(row['vitamin_a'])
+                    vitamin_c=float(row['vitamin_c'])
+                    vitamin_d=float(row['vitamin_d'])
 
-            meal_items.append(MealItemSpec(id=id, category=category, portion_volume=portion_volume,
-            cafeteria_id=cafeteria_id, max_pieces=max_pieces, carbohydrate=carbohydrate, calories=calories, protein=protein, 
-            total_fat=total_fat, saturated_fat=saturated_fat, sugar=sugar, cholesterol=cholesterol,
-            fiber=fiber, sodium=sodium, potassium=potassium,iron=iron, vitamin_a=vitamin_a,
-            vitamin_c=vitamin_c, vitamin_d=vitamin_d))
+                    meal_items.append(MealItemSpec(id=id, category=category, portion_volume=portion_volume,
+                    cafeteria_id=cafeteria_id, max_pieces=max_pieces, carbohydrate=carbohydrate, calories=calories, protein=protein, 
+                    total_fat=total_fat, saturated_fat=saturated_fat, sugar=sugar, cholesterol=cholesterol,
+                    fiber=fiber, sodium=sodium, potassium=potassium,iron=iron, vitamin_a=vitamin_a,
+                    vitamin_c=vitamin_c, vitamin_d=vitamin_d))
+
+                algo_state = item_choice_example(height, weight, birthdate, meals, meal_length, sex, health_goal, activity_level, meal_items)
+                #print(algo_state)
+                num_combination = 1
+
+                combinations = []
+                meal_length = 50
+                for i in algo_state.keys():
+                    num_combination *= len(algo_state[i]['items'])
+
+                for large_id in algo_state['large']['items']:
+                    for small1_id in algo_state['small1']['items']:
+                        for small2_id in algo_state['small2']['items']:
+                            combinations.append([large_id, small1_id, small2_id])
+                
+                for combination_count,combination in enumerate(combinations):          
+                    state=[]
+                    print(combination)
+                    for ind_combination, id_combination in enumerate(combination):
+                        item_index = short_list_df[short_list_df.pk == id_combination].index
+
+                        if float(short_list_df.loc[item_index,'portion_volume']) < 0:
+                            discrete=True
+                        else:
+                            discrete=False
+                        if ind_combination == 0:
+                            if discrete:
+                                max_volume=int(short_list_df.loc[item_index,'max_pieces'])
+                                portion_volume=int(short_list_df.loc[item_index, 'portion_volume'])*-1
+                            else:
+                                max_volume=610
+                                portion_volume = float(short_list_df.loc[item_index, 'portion_volume'])
+                        else:
+                            if discrete:
+                                max_volume = int(short_list_df.loc[item_index, 'max_pieces'])
+                                portion_volume=int(short_list_df.loc[item_index, 'portion_volume'])*-1
+                            else:
+                                max_volume=270
+                                portion_volume=float(short_list_df.loc[item_index, 'portion_volume'])
+
+                        carbohydrate = float(short_list_df.loc[item_index,'carbohydrate'])
+                        calories = float(short_list_df.loc[item_index,'calories'])
+                        protein = float(short_list_df.loc[item_index,'protein'])
+                        total_fat = float(short_list_df.loc[item_index,'total_fat'])
+                        saturated_fat = float(short_list_df.loc[item_index,'saturated_fat'])
+                        sugar= float(short_list_df.loc[item_index,'sugar'])
+                        cholesterol=float(short_list_df.loc[item_index,'cholesterol'])
+                        fiber= float(short_list_df.loc[item_index,'fiber'])
+                        sodium=float(short_list_df.loc[item_index,'sodium'])
+                        potassium=float(short_list_df.loc[item_index,'potassium'])
+                        calcium=float(short_list_df.loc[item_index,'calcium'])
+                        iron=float(short_list_df.loc[item_index,'iron'])
+                        vitamin_a=float(short_list_df.loc[item_index,'vitamin_a'])
+                        vitamin_c=float(short_list_df.loc[item_index,'vitamin_c'])
+                        vitamin_d=float(short_list_df.loc[item_index,'vitamin_d'])                    
+                        
+                        nutrition = Nutrition(calories=calories,carbohydrate=carbohydrate,protein=protein,
+                        total_fat=total_fat,saturated_fat=saturated_fat,trans_fat=0,sugar=sugar,cholesterol=cholesterol,
+                        fiber=fiber,sodium=sodium,potassium=potassium,calcium=calcium,iron=iron,vitamin_a=vitamin_a,
+                        vitamin_c=vitamin_c,vitamin_d=vitamin_d)
+
+                        state.append(PlateSectionState(discrete=discrete, id=id_combination, max_volume=max_volume, 
+                        portion_volume=portion_volume, nutrition=nutrition))
+                        print(portion_volume)
+                    
+                    algo_portion_state = item_portion_example(height, weight, birthdate, meals, meal_length, sex, health_goal, activity_level, state)
+                    #print('Result Combination:', combination_count+1)
+
+                    #print(algo_portion_state)
+                    d = [ind_student, sex, height, weight, health_goal, activity_level, age, day, meal_name, num_combination, combination_count+1, combination]
+                    d += retrieve_result(algo_portion_state)
+
+                    DATA.append(d)
+            else:
+                pass
+    print(DATA)
+    DATA_df = pd.DataFrame(DATA, columns=['Student_Number', 'Sex', 'Height', 'Weight', 'Health_Goal', 'Activity', 'Age', 
+    'Date', 'Meal_Name','Total_Combination', 'Current_Combination', 'Combination', 
+    'Vol_Large', 'Vol_Small1', 'Vol_Small2']+NUTRIENTS)
+    DATA_df.to_csv('test.csv', index=False)
 
 
-        algo_state = item_choice_example(height, weight, birthdate, meals, meal_length, sex, health_goal, activity_level, meal_items)
-        print(algo_state)
-    else:
-        pass
+   
+ 
